f8e43fad06b4263f138bcf94d662d96d8b2b30ae
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-06 13:38:57 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 5
+// store the famous author name this question related to Q4
+let famous_person = "Ralph Waldo Emerson";
+// let store the message
+let message = "once said,life is a journey,not a destination";
+console.log(famous_person, message);
---
+++ Q5.ts
@@ -0,0 +1,6 @@
+// Question 5
+// store the famous author name this question related to Q4
+let famous_person:string = "Ralph Waldo Emerson";
+// let store the message
+let message: string = "once said,life is a journey,not a destination"
+console.log(famous_person,message);
